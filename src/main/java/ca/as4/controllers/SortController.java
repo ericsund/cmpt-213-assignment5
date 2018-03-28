bb82e1cc3987ac01ff7a1a658ab9ff29a981445c
Eric Sund <epsund@gmail.com>
2018-03-28 01:34:16 -0700

    printing is working, just need to fix lecture capacity totals

--- src/main/java/ca/as4/controllers/SortController.java
+++ src/main/java/ca/as4/controllers/SortController.java
@@ -36,8 +36,8 @@ public class SortController {
 
         ArrayList<Data>[] fixedListClasses = fixArrayListSize(organizeClasses);
         sortEachArrayList(fixedListClasses);
-        displayClassData(fixedListClasses);
-//        printDump(groupByClass(organizeClasses));
+//        displayClassData(fixedListClasses);
+        printDump(organizeClasses);
     }
 
     private ArrayList<Data>[] fixArrayListSize(ArrayList<Data>[] organizedClasses)
@@ -214,22 +214,313 @@ public class SortController {
 //        return groupedData;
 //    }
 //
-//    private void printDump(ArrayList<ArrayList<ArrayList<Data>>> groupedData) {
-//        for (ArrayList<ArrayList<Data>> currentClassSet : groupedData) {
-//
-//            System.out.println(currentClassSet.get(0).get(0).getSubject() + " " +
-//                    currentClassSet.get(0).get(0).getCatalogNumber());
-//
-//            for (ArrayList<Data> currentClassSemester : currentClassSet) {
-//                for (Data currentClass : currentClassSemester) {
-//
-//                    System.out.println("\t" + currentClass.getSemester() + " in " + currentClass.getLocation() +
-//                    " by " + currentClass.getInstructors());
-//                    System.out.println("\tType=" + currentClass.getComponentCode() + ", Enrollment=blankfornow");
-//                }
-//            }
-//        }
-//    }
+
+    // todo move a lot of this code into private functions to clean it up!
+    //Debug: Display the class data in their grouped order
+    private void printDump(ArrayList<Data>[] organizedData) {
+
+        for (ArrayList<Data> currentList : organizedData) {
+            if (currentList.size() > 0) {
+
+                // create an ArrayList to hold our instructors
+                ArrayList<String> instructors = new ArrayList<>();
+                // hold enrollments totals
+                int enrollmentTotalLab = 0;
+                int enrollmentTotalLec = 0;
+                int enrollmentTotalOpl = 0;
+                int enrollmentTotalTut = 0;
+                int enrollmentTotalWks = 0;
+                // hold enrollment capacities
+                int enrollmentCapacityLec = 0;
+                int enrollmentCapacityLab = 0;
+                int enrollmentCapacityOpl = 0;
+                int enrollmentCapacityTut = 0;
+                int enrollmentCapacityWks = 0;
+
+                boolean lab = false;
+                boolean lec = false;
+                boolean opl = false;
+                boolean tut = false;
+                boolean wks = false;
+
+                // print course group
+                System.out.println(currentList.get(0).getSubject() + " " +
+                        currentList.get(0).getCatalogNumber());
+
+                for (int j = 0; j < currentList.size(); j++) {
+
+                    // get current semester, current location
+                    int currentSem = currentList.get(j).getSemester();
+                    String currentLoc = currentList.get(j).getLocation();
+
+                    // check if the current semester is the same as the next one
+                    // keep checking unless we reach the end of the ArrayList
+                    if (j + 1 < currentList.size()) {
+
+                        if (currentSem == currentList.get(j + 1).getSemester()) {
+
+                            if (currentLoc.equals(currentList.get(j + 1).getLocation())) {
+                                // grab all the instructors in a class' semester's location
+                                // add those to our list who are not already in it
+                                ArrayList<String> currentInstructors =  currentList.get(j).getInstructors();
+                                for (String currentInstructor : currentInstructors) {
+                                    if (!(instructors.contains(currentInstructor))) {
+                                        instructors.add(currentInstructor);
+                                    }
+                                }
+
+                                // add up all lab data for a class' semester's location
+                                if (currentList.get(j).getComponentCode().equals("LAB")) {
+                                    enrollmentTotalLab += currentList.get(j).getEnrollmentTotal();
+                                    enrollmentCapacityLab += currentList.get(j).getEnrollmentCapacity();
+                                    lab = true;
+                                }
+
+                                // add up all lec data for a class' semester's location
+                                if (currentList.get(j).getComponentCode().equals("LEC")) {
+                                    enrollmentTotalLec += currentList.get(j).getEnrollmentTotal();
+                                    enrollmentCapacityLec += currentList.get(j).getEnrollmentCapacity();
+                                    lec = true;
+                                }
+
+                                // add up all pol data for a class' semester's location
+                                if (currentList.get(j).getComponentCode().equals("OPL")) {
+                                    enrollmentTotalOpl += currentList.get(j).getEnrollmentTotal();
+                                    enrollmentCapacityOpl += currentList.get(j).getEnrollmentCapacity();
+                                    opl = true;
+                                }
+
+                                // add up all tut data for a class' semester's location
+                                if (currentList.get(j).getComponentCode().equals("TUT")) {
+                                    enrollmentTotalTut += currentList.get(j).getEnrollmentTotal();
+                                    enrollmentCapacityTut += currentList.get(j).getEnrollmentCapacity();
+                                    tut = true;
+                                }
+
+                                // add up all opl data for a class' semester's location
+                                if (currentList.get(j).getComponentCode().equals("WKS")) {
+                                    enrollmentTotalWks += currentList.get(j).getEnrollmentTotal();
+                                    enrollmentCapacityWks += currentList.get(j).getEnrollmentCapacity();
+                                    wks = true;
+                                }
+
+                            }
+
+                            // currentLoc != currentLoc+1
+                            else {
+                                System.out.println("\t" + currentSem + " in " + currentLoc + " by " +
+                                        instructors.toString());
+
+                                if (lab) {
+                                    System.out.println("\t\t" + "Type=LAB, Enrollment=" + enrollmentTotalLab +
+                                            "/" + enrollmentCapacityLab);
+
+                                    enrollmentTotalLab = 0;
+                                    enrollmentCapacityLab = 0;
+                                    lab = false;
+                                }
+
+                                if (lec) {
+                                    System.out.println("\t\t" + "Type=LEC, Enrollment=" + enrollmentTotalLec +
+                                            "/" + enrollmentCapacityLec);
+
+                                    enrollmentTotalLec = 0;
+                                    enrollmentCapacityLec = 0;
+                                    lec = false;
+                                }
+
+                                if (opl) {
+                                    System.out.println("\t\t" + "Type=OPL, Enrollment=" + enrollmentTotalOpl +
+                                            "/" + enrollmentCapacityOpl);
+
+                                    enrollmentTotalOpl = 0;
+                                    enrollmentCapacityOpl = 0;
+                                    opl = false;
+                                }
+
+                                if (tut) {
+                                    System.out.println("\t\t" + "Type=TUT, Enrollment=" + enrollmentTotalTut +
+                                            "/" + enrollmentCapacityTut);
+
+                                    enrollmentTotalTut = 0;
+                                    enrollmentCapacityTut = 0;
+                                    tut = false;
+                                }
+
+                                if (wks) {
+                                    System.out.println("\t\t" + "Type=WKS, Enrollment=" + enrollmentTotalWks +
+                                            "/" + enrollmentCapacityWks);
+
+                                    enrollmentTotalWks = 0;
+                                    enrollmentCapacityWks = 0;
+                                    wks = false;
+                                }
+
+                                instructors.clear();
+                            }
+
+                        }
+
+                        // currentSem != currentSem+1
+                        else {
+                            System.out.println("\t" + currentSem + " in " + currentLoc + " by " +
+                                    instructors.toString());
+
+                            if (lab) {
+                                System.out.println("\t\t" + "Type=LAB, Enrollment=" + enrollmentTotalLab +
+                                        "/" + enrollmentCapacityLab);
+
+                                enrollmentTotalLab = 0;
+                                enrollmentCapacityLab = 0;
+                                lab = false;
+                            }
+
+                            if (lec) {
+                                System.out.println("\t\t" + "Type=LEC, Enrollment=" + enrollmentTotalLec +
+                                        "/" + enrollmentCapacityLec);
+
+                                enrollmentTotalLec = 0;
+                                enrollmentCapacityLec = 0;
+                                lec = false;
+                            }
+
+                            if (opl) {
+                                System.out.println("\t\t" + "Type=OPL, Enrollment=" + enrollmentTotalOpl +
+                                        "/" + enrollmentCapacityOpl);
+
+                                enrollmentTotalOpl = 0;
+                                enrollmentCapacityOpl = 0;
+                                opl = false;
+                            }
+
+                            if (tut) {
+                                System.out.println("\t\t" + "Type=TUT, Enrollment=" + enrollmentTotalTut +
+                                        "/" + enrollmentCapacityTut);
+
+                                enrollmentTotalTut = 0;
+                                enrollmentCapacityTut = 0;
+                                tut = false;
+                            }
+
+                            if (wks) {
+                                System.out.println("\t\t" + "Type=WKS, Enrollment=" + enrollmentTotalWks +
+                                        "/" + enrollmentCapacityWks);
+
+                                enrollmentTotalWks = 0;
+                                enrollmentCapacityWks = 0;
+                                wks = false;
+                            }
+
+                            instructors.clear();
+                        }
+                    }
+
+                    // todo this might be missing the calculation of the last component codes...
+                    // reached end of ArrayList size, print last element we had
+                    else {
+
+                        // grab all the instructors in a class' semester's location
+                        // add those to our list who are not already in it
+                        ArrayList<String> currentInstructors =  currentList.get(j).getInstructors();
+                        for (String currentInstructor : currentInstructors) {
+                            if (!(instructors.contains(currentInstructor))) {
+                                instructors.add(currentInstructor);
+                            }
+                        }
+
+                        // add up all lab data for a class' semester's location
+                        if (currentList.get(j).getComponentCode().equals("LAB")) {
+                            enrollmentTotalLab += currentList.get(j).getEnrollmentTotal();
+                            enrollmentCapacityLab += currentList.get(j).getEnrollmentCapacity();
+                            lab = true;
+                        }
+
+                        // add up all lec data for a class' semester's location
+                        if (currentList.get(j).getComponentCode().equals("LEC")) {
+                            enrollmentTotalLec += currentList.get(j).getEnrollmentTotal();
+                            enrollmentCapacityLec += currentList.get(j).getEnrollmentCapacity();
+                            lec = true;
+                        }
+
+                        // add up all pol data for a class' semester's location
+                        if (currentList.get(j).getComponentCode().equals("OPL")) {
+                            enrollmentTotalOpl += currentList.get(j).getEnrollmentTotal();
+                            enrollmentCapacityOpl += currentList.get(j).getEnrollmentCapacity();
+                            opl = true;
+                        }
+
+                        // add up all tut data for a class' semester's location
+                        if (currentList.get(j).getComponentCode().equals("TUT")) {
+                            enrollmentTotalTut += currentList.get(j).getEnrollmentTotal();
+                            enrollmentCapacityTut += currentList.get(j).getEnrollmentCapacity();
+                            tut = true;
+                        }
+
+                        // add up all opl data for a class' semester's location
+                        if (currentList.get(j).getComponentCode().equals("WKS")) {
+                            enrollmentTotalWks += currentList.get(j).getEnrollmentTotal();
+                            enrollmentCapacityWks += currentList.get(j).getEnrollmentCapacity();
+                            wks = true;
+                        }
+
+
+                        System.out.println("\t" + currentSem + " in " + currentLoc + " by " +
+                                instructors.toString());
+
+                        if (lab) {
+                            System.out.println("\t\t" + "Type=LAB, Enrollment=" + enrollmentTotalLab +
+                                    "/" + enrollmentCapacityLab);
+
+                            enrollmentTotalLab = 0;
+                            enrollmentCapacityLab = 0;
+                            lab = false;
+                        }
+
+                        if (lec) {
+                            System.out.println("\t\t" + "Type=LEC, Enrollment=" + enrollmentTotalLec +
+                                    "/" + enrollmentCapacityLec);
+
+                            enrollmentTotalLec = 0;
+                            enrollmentCapacityLec = 0;
+                            lec = false;
+                        }
+
+                        if (opl) {
+                            System.out.println("\t\t" + "Type=OPL, Enrollment=" + enrollmentTotalOpl +
+                                    "/" + enrollmentCapacityOpl);
+
+                            enrollmentTotalOpl = 0;
+                            enrollmentCapacityOpl = 0;
+                            opl = false;
+                        }
+
+                        if (tut) {
+                            System.out.println("\t\t" + "Type=TUT, Enrollment=" + enrollmentTotalTut +
+                                    "/" + enrollmentCapacityTut);
+
+                            enrollmentTotalTut = 0;
+                            enrollmentCapacityTut = 0;
+                            tut = false;
+                        }
+
+                        if (wks) {
+                            System.out.println("\t\t" + "Type=WKS, Enrollment=" + enrollmentTotalWks +
+                                    "/" + enrollmentCapacityWks);
+
+                            enrollmentTotalWks = 0;
+                            enrollmentCapacityWks = 0;
+                            wks = false;
+                        }
+
+                        instructors.clear();
+                    }
+                }
+            }
+
+            // place newline after current course group
+            System.out.println();
+        }
+    }
 
     private String displayAllInstructors(Data classFileWithInstructors)
     {
